a031d1c87928948acbf61238498f35ee827e27f9
“Kuwado” <viethoan557723@gmail.com>
2025-02-17 17:26:38 +0700

    first commit
    Merged main and updated project
    
    merge
    
    Merged main and updated project
    
    Django Tutorial Part 2: Creating a skeleton website# This is a combination of 3 commits.
    
    first commit
    
    test
    
    Django Tutorial Part 2: Creating a skeleton website
    
    Django Tutorial Part 2: Creating a skeleton website
    
    Django Tutorial Part 2: Creating a skeleton website
    
    Django Tutorial Part 2: Creating a skeleton website
    
    Django Tutorial Part 2: Creating a skeleton website#
    
    remove gitignore
    
    test
    
    Django Tutorial Part 2: Creating a skeleton website
    
    Django Tutorial Part 3: Using models
    
    remove gitignore
    
    i18n
    
    update po
    
    update
    
    update
    
    Merged main and updated project + merge 4
    
    uodate readme
    
    update

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+# Đăng ký các models
+from .models import Author, Genre, Book, BookInstance
+
+
+# Define the admin class
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "display_genre")
+    # Thêm BookInstance vào dưới Book
+    inlines = [BooksInstanceInline]
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ("status", "due_back")
+
+    # Thêm section trong xem chi tiết
+    fieldsets = (
+        (None, {"fields": ("book", "imprint", "id")}),
+        ("Availability", {"fields": ("status", "due_back")}),
+    )
